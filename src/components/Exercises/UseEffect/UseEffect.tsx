805241c983d0bc4a277399c6c197c060e4e271a1
Sabina Mejlisson <sabinamejlisson@gmail.com>
2025-03-01 23:25:47 +0100

    Add useEffect hook documentation with usage examples

---
+++ src/components/Exercises/UseEffect/UseEffect.tsx
@@ -0,0 +1,6 @@
+//useEffect() = React Hook that tells React do some code when : 
+            //  useEffect(() => {})              Runs after every re-render
+            //  useEffect(() => {}, [])          Runs only on mount
+            //  useEffect(() => {}, [value])     Runs on mount + when value changes
+
+
